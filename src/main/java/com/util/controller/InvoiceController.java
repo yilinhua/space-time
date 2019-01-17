f53894011415402517a6825851759bef6fe064ef
yilinhua <18216393925.163.com>
2019-01-17 08:39:27 +0800

    yilinhua

--- src/main/java/com/util/controller/InvoiceController.java
+++ src/main/java/com/util/controller/InvoiceController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.elasticsearch.core.ResultsMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
